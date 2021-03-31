2d8b49706bc1b21f80c818c9519eae316bab5292
jinhyuck <ckwlsgur20@naver.com>
2021-03-31 14:15:53 +0900

    train, test integration

--- 4.pytorch-mnist/build.sh
+++ 4.pytorch-mnist/build.sh
@@ -1,12 +1,7 @@
 img_name=mnist
-ver=1.5v
+ver=1.7v
 
 # train
-docker build -t "${img_name}_train:$ver" ./train
-docker tag "${img_name}_train:$ver" "ckwlsgur20/${img_name}_train:$ver"
-docker push ckwlsgur20/"${img_name}_train:$ver"
-
-# test
-docker build -t "${img_name}_test:$ver" ./test
-docker tag "${img_name}_test:$ver" "ckwlsgur20/${img_name}_test:$ver"
-docker push ckwlsgur20/"${img_name}_test:$ver"
+docker build -t "${img_name}:$ver" .
+docker tag "${img_name}:$ver" "ckwlsgur20/${img_name}:$ver"
+docker push ckwlsgur20/"${img_name}:$ver"
--- 4.pytorch-mnist/main.py
+++ 4.pytorch-mnist/main.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 import argparse
 import torch
 import torch.nn as nn
@@ -51,11 +50,32 @@ def train(args, model, device, train_loader, optimizer, epoch):
                 break
 
 
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
     parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                         help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
     parser.add_argument('--epochs', type=int, default=14, metavar='N',
                         help='number of epochs to train (default: 14)')
     parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
@@ -70,8 +90,6 @@ def main():
                         help='random seed (default: 1)')
     parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                         help='how many batches to wait before logging training status')
-    parser.add_argument('--save-model', action='store_true', default=False,
-                        help='For Saving the current Model')
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
 
@@ -80,19 +98,24 @@ def main():
     device = torch.device("cuda" if use_cuda else "cpu")
 
     train_kwargs = {'batch_size': args.batch_size}
+    test_kwargs = {'batch_size': args.test_batch_size}
     if use_cuda:
         cuda_kwargs = {'num_workers': 1,
                        'pin_memory': True,
                        'shuffle': True}
         train_kwargs.update(cuda_kwargs)
+        test_kwargs.update(cuda_kwargs)
 
     transform=transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
         ])
-
-    dataset1 = datasets.MNIST('/mnt/data', train=True, download=False, transform=transform)
+    dataset1 = datasets.MNIST('/mnt/data', train=True, download=False,
+                              transform=transform)
+    dataset2 = datasets.MNIST('/mnt/data', train=False,
+                              transform=transform)
     train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
+    test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
     model = Net().to(device)
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
@@ -100,10 +123,10 @@ def main():
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch)
+        test(model, device, test_loader)
         scheduler.step()
 
-    if args.save_model:
-        torch.save(model.state_dict(), "mnist_cnn.pt")
+    torch.save(model.state_dict(), "mnist_cnn.pt")
 
 
 if __name__ == '__main__':
