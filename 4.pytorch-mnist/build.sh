bdab453ff0f335f895054e6637ecda563ebabbe3
jinhyuck <ckwlsgur20@naver.com>
2021-03-31 11:39:02 +0900

    init

---
+++ 4.pytorch-mnist/build.sh
@@ -0,0 +1,12 @@
+img_name=mnist
+ver=1.0v
+
+# train
+docker build -t "${img_name}_train:$ver" ./train
+docker tag "${img_name}_train:$ver" "ckwlsgur20/${img_name}_train:$ver"
+docker push ckwlsgur20/"${img_name}_train:$ver"
+
+# test
+docker build -t "${img_name}_test:$ver" ./test
+docker tag "${img_name}_test:$ver" "ckwlsgur20/${img_name}_test:$ver"
+docker push ckwlsgur20/"${img_name}_test:$ver"
