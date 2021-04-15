617ed0aab8a2aee339a5aa789b192e844392cf8d
jinhyuck <ckwlgsur20@gmail.com>
2021-04-15 09:21:09 +0900

    5.advanced deleted

--- 5.advanced/train-test/build.sh
+++
@@ -1,7 +0,0 @@
-registry=ckwlsgur20
-img_name=mnist
-ver=4.0v
-
-docker build -t "${img_name}:$ver" .
-docker tag "${img_name}:$ver" "${registry}/${img_name}:$ver"
-docker push "${registry}/${img_name}:{$ver}"
